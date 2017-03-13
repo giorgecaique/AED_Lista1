0cd8274d74feab387b6c7bb34f365106dd8c79b3
Giorge Caique Pinheiro de Souza Luiz <giorgecaique@hotmail.com>
2017-03-12 21:57:27 -0300

    Alteração no cabeçalho.

--- AED1/Cabecalho.cs
+++ AED1/Cabecalho.cs
@@ -10,7 +10,8 @@ namespace AED1
     {
         static public void Print(string exercicio, TimeSpan tempoIterativo, TimeSpan tempoRecursivo, int operacoesIterativo, int operacoesRecursivo, object resultIterativo, object resultRecursivo)
         {
-            Console.WriteLine(@"Sistemas de Informação - Algoritmos e Estrutura de Dados
+            Console.WriteLine(@"--------------------------------------------------------
+Sistemas de Informação - Algoritmos e Estrutura de Dados
 --------------------------------------------------------
 Aluno:     Giorge Caique Pinheiro de Souza Luiz
 Matrícula: 548749
@@ -28,7 +29,7 @@ Recursivo : {4}
 Resultado
 Iterativo : {5}
 Recursivo : {6}
-
+--------------------------------------------------------
 Pressione qualquer tecla para sair", exercicio, tempoIterativo, tempoRecursivo, operacoesIterativo, operacoesRecursivo, resultIterativo, resultRecursivo);
             Console.ReadKey();
         }
